d818071d243cb1d0a6f7c08851071d513f3fc1b8
Mominul <hussainmominul786@gmail.com>
2025-04-04 07:44:21 +0530

    added auth and message container

--- src/App.js
+++ src/App.js
@@ -29,6 +29,9 @@ const auth = firebase.auth();
 const firestore = firebase.firestore();
 
 function App() {
+
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -36,8 +39,58 @@ function App() {
       <SignOut />
         
       </header>
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
     </div>
   );
+}
+
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+
+
+  return(
+    <button onclick={signInWithGoogle}>Sign in with Google</button>
+  )
+}
+
+function SignOut() {
+
+
+  return auth.currentUser && (
+    <button onClick={() => auth.signOut()}>Sign Out</button>
+  )
+}
+
+function ChatRoom() {
+  const messagesRef = firestore.collection('messages');
+  const query = messagesRef.orderBy('createdAt').limit(25);
+
+  const [messages] = useCollectionData(query, { idField: 'id' });
+
+  return (
+    <>
+      <div>
+        {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+      </div>
+      <ChatForm />
+    </>
+  )
+}
+
+function ChatMessage(props) {
+  const { text, uid } = props.message;
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+
+  return (
+    <div className={`message ${messageClass}`}>
+      <p>{text}</p>
+    </div>
+  )
 }
 
 export default App;
